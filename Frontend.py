17c3add50b9b0fe0c5e04cdcd241e46d056f84a9
phenogood <74715388+phenogood@users.noreply.github.com>
2024-10-10 14:07:23 +0700

    Frontend Foundation

---
+++ Frontend.py
@@ -0,0 +1,14 @@
+# MODULES
+from tkinter import * #type: ignore
+from matplotlib import * #type: ignore
+
+"""
+    REMINDER:
+    - DO NOT FORGET TO INSTALL TKINTER & MATPLOTLIB IN YOUR MACHINE!
+    - INSTALL IN CMD WITH: 'pip install {module}' / 'pip3 install {module}' /
+            'python -m pip install {module}' / 'python3 -m pip install {module}'.
+"""
+
+# WORKSPACE
+
+
