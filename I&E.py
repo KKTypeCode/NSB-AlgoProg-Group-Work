263c3b45eeddfa37b95fb4e94be87b7d68dd428c
phenogood <74715388+phenogood@users.noreply.github.com>
2024-10-13 15:01:25 +0700

    Identifier Generation for Entries with I&E

--- I&E.py
+++ I&E.py
@@ -1,5 +1,6 @@
 from openpyxl import * #type: ignore
 from openpyxl.utils import get_column_letter
+import datetime as dt
 
 """
     REMINDER:
@@ -11,8 +12,33 @@ from openpyxl.utils import get_column_letter
     - CODE CANNOT WORK WHEN EXCEL FILE IS BEING OPENED.
 """
 
-# ADD NEW ENTRY FOR INCOME & EXPENSE
+# GENERATE A SPECIAL ID KEY TO ALLOW DIFFERENCE BETWEEN ENTRIES
 
+def gen_id_key():
+    with open('NSB-AlgoProg-Group-Work/Set_Const.txt', 'r') as file:
+        current_value = int(file.readline().strip().split('ID ')[-1])
+        new_value = current_value + 1
+        id_key = f"{current_value:06d}"
+    
+    with open('NSB-AlgoProg-Group-Work/Set_Const.txt', 'w') as file:
+        file.seek(0)
+        file.write(f"ID {new_value:06d}")
+        
+    return id_key
+
+# GENERATE CURRENT DATE AND TIME
+    
+def date():
+    current_date = dt.datetime.now().date()
+    return current_date.strftime("%Y-%m-%d")
+
+def time():
+    timezone = dt.timezone(dt.timedelta(hours=7))
+    current_time = dt.datetime.now(timezone).time()
+    return current_time.strftime("%H:%M:%S")
+
+# ADD INCOME AND EXPENSE ENTRIES TO THE DATABASE
+    
 def income(name, value=0, category='income', delivered=False):
 	wb = load_workbook('NSB-AlgoProg-Group-Work/Database.xlsx')
 	db = wb.active 
@@ -23,11 +49,14 @@ def income(name, value=0, category='income', delivered=False):
 		row += 1
 
 	db[get_column_letter(col) + str(row)] = name.upper()
-	db[get_column_letter(col+1) + str(row)] = 'I'
-	db[get_column_letter(col+2) + str(row)] = value
-	db[get_column_letter(col+3) + str(row)] = category.upper()
-	db[get_column_letter(col+4) + str(row)] = delivered
-	
+	db[get_column_letter(col+1) + str(row)] = date()
+	db[get_column_letter(col+2) + str(row)] = 'I'
+	db[get_column_letter(col+3) + str(row)] = value
+	db[get_column_letter(col+4) + str(row)] = category.upper()
+	db[get_column_letter(col+5) + str(row)] = delivered
+	db[get_column_letter(col+6) + str(row)] = gen_id_key()
+	db[get_column_letter(col+7) + str(row)] = f"{date()} / {time()}"
+
 	wb.save('NSB-AlgoProg-Group-Work/Database.xlsx')
 
 def expense(name, value=0, category='expense', delivered=False):
@@ -40,31 +69,27 @@ def expense(name, value=0, category='expense', delivered=False):
 		row += 1
 
 	db[get_column_letter(col) + str(row)] = name.upper()
-	db[get_column_letter(col+1) + str(row)] = 'E'
-	db[get_column_letter(col+2) + str(row)] = value
-	db[get_column_letter(col+3) + str(row)] = category.upper()
-	db[get_column_letter(col+4) + str(row)] = delivered
-          
+	db[get_column_letter(col+1) + str(row)] = date()
+	db[get_column_letter(col+2) + str(row)] = 'E'
+	db[get_column_letter(col+3) + str(row)] = value
+	db[get_column_letter(col+4) + str(row)] = category.upper()
+	db[get_column_letter(col+5) + str(row)] = delivered
+	db[get_column_letter(col+6) + str(row)] = gen_id_key()
+	db[get_column_letter(col+7) + str(row)] = f"{date()} / {time()}"
+    
 	wb.save('NSB-AlgoProg-Group-Work/Database.xlsx')
+
+# MODIFY ENTRIES IN THE DATABASE
 
 def read():
     wb = load_workbook('NSB-AlgoProg-Group-Work/Database.xlsx')
     db = wb.active
     
-    for row in db.iter_rows(max_col=6, min_row=2, max_row=3):
+    for row in db.iter_rows(max_col=8, min_row=2, max_row=db.max_row):
         for cell in row:
             if cell.value != None:
                 print(cell.value, end=" ")
         print('', end="\n")
-
-def get_entry():
-    wb = load_workbook('NSB-AlgoProg-Group-Work/Database.xlsx')
-    db = wb.active
-    
-    for row in db.iter_cols(min_col=1, max_col=6, min_row=2):
-        for cell in row:
-            if cell.value != None:
-                print(cell.value)
 
 def change(name=None, ie=None, value=None, category=None, delivered=None):
     wb = load_workbook('NSB-AlgoProg-Group-Work/Database.xlsx')
@@ -84,7 +109,7 @@ def change(name=None, ie=None, value=None, category=None, delivered=None):
         print('Entry does not exist.')
     else:
         index = entry_list.index(selected) + 2 
-        db[get_column_letter(2) + str(index)] = name.upper if name != None else db[get_column_letter(2) + str(index)].value
+        db[get_column_letter(2) + str(index)] = name if name != None else db[get_column_letter(2) + str(index)].value
         db[get_column_letter(3) + str(index)] = ie.upper() if ie != None else db[get_column_letter(3) + str(index)].value
         db[get_column_letter(4) + str(index)] = value if value != None else db[get_column_letter(4) + str(index)].value
         db[get_column_letter(5) + str(index)] = category.upper() if category != None else db[get_column_letter(5) + str(index)].value
@@ -92,5 +117,38 @@ def change(name=None, ie=None, value=None, category=None, delivered=None):
         
     read()
     wb.save('NSB-AlgoProg-Group-Work/Database.xlsx')
-  
+    
+def delete():
+    wb = load_workbook('NSB-AlgoProg-Group-Work/Database.xlsx')
+    db = wb.active
 
+    entry_list = []
+    
+    print("Available entries:")
+    for column in db.iter_cols(min_row=2, min_col=2, max_col=2):
+        for cell in column:
+            if cell.value != None:
+                entry_list.append(cell.value)
+
+    selected = str(input('Select entry to delete: ')).strip().upper()
+
+    if selected not in entry_list:
+        print('Entry does not exist.')
+    else:
+        index = entry_list.index(selected) + 2 
+        db[get_column_letter(2) + str(index)] = None
+        db[get_column_letter(3) + str(index)] = None
+        db[get_column_letter(4) + str(index)] = None
+        db[get_column_letter(5) + str(index)] = None
+        db[get_column_letter(6) + str(index)] = None
+        db[get_column_letter(7) + str(index)] = None
+        
+    read()
+    wb.save('NSB-AlgoProg-Group-Work/Database.xlsx')
+
+# TESTING
+income('Commission', 1000, delivered=True)
+
+# NOTE:
+# CODE HAS NOT BEEN FULLY TESTED YET // STILL NEED MORE TESTING
+# ADDITION OF BALANCE FUNCTIONALITY IS REQUIRED
