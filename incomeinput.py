ebfc12dcfcd73e9107d5abaa9a32f77dc3bc1d8d
FaultyDuck <alexanderrk03@gmail.com>
2024-10-23 16:50:20 +0700

    IncomepageRY

--- incomeinput.py
+++ incomeinput.py
@@ -1,28 +1,82 @@
 import tkinter as tk
+from tkinter import *
 
 def screenprnt():
+    name = namestring.get()
     income = incomeint.get()
-    print(income)
+    date = dateint.get()
+    rate = rateint.get()
+    cat = catstring.get()
+    complete1 = sel()
+    print(name,income,date,rate,cat,complete1)
+    namestring.set('')
     incomeint.set('')
+    dateint.set('')
+    rateint.set('')
+    catstring.set('')
+
+def sel():
+    complete = completionBool.get()
+    return complete
 
 root = tk.Tk()
 root.geometry("900x600")
 
+#Name Section
+namestring=tk.StringVar()
+namename = tk.Label(root, text = 'Input Name')
+nameentry = tk.Entry(root, textvariable= namestring)
+namename.pack(side = 'top',anchor = 'center', pady = 5)
+nameentry.pack(side = 'top',anchor = 'center', pady = 5)
+
+#Income Section
 incomeint=tk.StringVar()
-
-incomename = tk.Label(root, text = 'Input income')
+incomename = tk.Label(root, text = 'Input Income')
 incomeentry = tk.Entry(root, textvariable= incomeint)
+incomename.pack(side = 'top',anchor = 'center', pady = 5)
+incomeentry.pack(side = 'top',anchor = 'center', pady = 5)
+
+#Date Section
+dateint=tk.StringVar()
+datename = tk.Label(root, text = 'Input Date')
+dateentry = tk.Entry(root, textvariable= dateint)
+datename.pack(side = 'top',anchor = 'center', pady = 5)
+dateentry.pack(side = 'top',anchor = 'center', pady = 5)
+
+#Category Section
+catstring=tk.StringVar()
+catname = tk.Label(root, text = 'Input Category')
+catentry = tk.Entry(root, textvariable= catstring)
+catname.pack(side = 'top',anchor = 'center', pady = 5)
+catentry.pack(side = 'top',anchor = 'center', pady = 5)
+
+#Rate Section
+rateint=tk.StringVar()
+ratename = tk.Label(root, text = 'Input Rate/Date')
+rateentry = tk.Entry(root, textvariable= rateint)
+ratename.pack(side = 'top',anchor = 'center', pady = 5)
+rateentry.pack(side = 'top',anchor = 'center', pady = 5)
+
+#Completion of Transaction Section
+completionBool=tk.BooleanVar()
+completename = tk.Label(root, text = 'Input Completion of Transaction')
+completename.pack(side = 'top',anchor = 'center', pady = 5)
+R1 = Radiobutton(root, text='True',variable=completionBool, value=True, command=sel)
+R1.pack(side = 'top',anchor = 'center', pady=5)
+R2 = Radiobutton(root, text='False',variable=completionBool, value=False, command=sel)
+R2.pack(side = 'top',anchor = 'center', pady=5)
+
+
+#Button
 button = tk.Button(root, text='Enter', command = screenprnt)
-
-widthW = root.winfo_width()
-widthname = incomename.winfo_width()
-widthentry = incomeentry.winfo_width()
-widthbutton = button.winfo_width()
-
-x = (widthW - widthname) // 2
-
-incomename.pack(side = 'top',anchor = 'center', pady = 10)
-incomeentry.pack(side = 'top',anchor = 'center', pady = 10)
-button.pack(side = 'top',anchor = 'center', pady = 10)
+button.pack(side = 'top',anchor = 'center', pady = 5)
 
 root.mainloop()
+
+'''
+Name 
+Date 
+Value/Income
+Category 
+Completion of Transaction t/f
+'''
